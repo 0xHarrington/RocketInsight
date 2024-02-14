75f88e956299c6c4378c77a9b4d758fa2b60ce74
0xHarrington <harringtonswim@gmail.com>
2024-02-14 17:25:44 -0500

    starter components

---
+++ frontend/src/components/MarketAnalysis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MarketAnalysis() {
+    return (
+        <div>
+            <h1>Market Analysis</h1>
+            {/* Market analysis UI and logic will be implemented here */}
+        </div>
+    );
+}
+
+export default MarketAnalysis;
---
+++ frontend/src/components/UserInsights.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserInsights() {
+    return (
+        <div>
+            <h1>User Insights</h1>
+            {/* User insights UI and logic will be implemented here */}
+        </div>
+    );
+}
+
+export default UserInsights;
