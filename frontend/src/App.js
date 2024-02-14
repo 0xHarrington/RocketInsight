129db6543bc54d2e591e6701ff6fdd121249f66c
0xHarrington <harringtonswim@gmail.com>
2024-02-14 17:26:18 -0500

    Starter app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import MarketAnalysis from './components/MarketAnalysis';
+import UserInsights from './components/UserInsights';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Navigation Links can be added here */}
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/market-analysis" component={MarketAnalysis} />
+          <Route path="/user-insights" component={UserInsights} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
