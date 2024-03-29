c617dbc177f461d9e89e004462ccae8f81dcaa74
0xHarrington <harringtonswim@gmail.com>
2024-03-29 16:20:46 -0400

    Routing with react-router

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NavBar = ({ showpage, handlepage }) => (
+const NavBar = () => (
     <nav>
         <div className="marginals">
             <div className="VTEE">RocketInsight</div>
             <ul>
-                <li>
-                    <button className={showpage === 1 ? "nav-link active" : "nav-link"} onClick={() => handlepage(1)}>Markets</button>
-                </li>
-                <li>
-                    <button className={showpage === 2 ? "nav-link active" : "nav-link"} onClick={() => handlepage(2)}>Wallets</button>
-                </li>
+                <li><Link to="/markets" className="nav-link">Markets</Link></li>
+                <li><Link to="/wallets" className="nav-link">Wallets</Link></li>
             </ul>
-            <div className="login">
+            <div className="alternative">
                 <ul>
                     <li><a href="#">FAQ</a></li>
                     <li><a href="#">Contact Us</a></li>
