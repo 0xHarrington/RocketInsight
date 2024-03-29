6843e38867b0fe68c3053659d1d1475f76b4a683
0xHarrington <harringtonswim@gmail.com>
2024-03-29 16:20:17 -0400

    bootstrapped UI

---
+++ frontend/src/pages/MarketPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LineGraph from '../components/Graph/LineGraph';
+
+const MarketPage = () => (
+    <div>
+        {/* Market-specific content */}
+        <h1>Market Overview</h1>
+        <LineGraph />
+        {/* Any additional content */}
+    </div>
+);
+
+export default MarketPage;
---
+++ frontend/src/pages/WalletPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import WalletDetails from '../components/WalletDetails';
+
+const WalletPage = () => (
+    <>
+        <h1>Wallet address clustering</h1>
+        <WalletDetails />
+    </>
+);
+
+export default WalletPage;
