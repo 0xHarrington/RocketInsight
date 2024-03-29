290e21caeee667f8b68d42bfd68f70c0cec4535a
0xHarrington <harringtonswim@gmail.com>
2024-03-29 16:56:03 -0400

    bootstrapping wallet details

--- frontend/src/pages/WalletPage.js
+++ frontend/src/pages/WalletPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import WalletDetails from '../components/WalletDetails';
+import WalletDetails from '../WalletDetails';
 
 const WalletPage = () => (
-    <>
+    <div>
         <h1>Wallet address clustering</h1>
         <WalletDetails />
-    </>
+    </div>
 );
 
 export default WalletPage;
