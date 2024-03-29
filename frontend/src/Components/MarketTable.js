537471ab531d10a1db3aa4aaf489fb32f9a07980
0xHarrington <harringtonswim@gmail.com>
2024-03-29 18:53:47 -0400

    get rid of showpage artifacts

--- frontend/src/Components/MarketTable.js
+++ frontend/src/Components/MarketTable.js
@@ -29,16 +29,8 @@ const MarketTable = () => (
                     Supply APY
                 </div>
             </td>
-            <td>
-                <div className="thead">
-                    Borrow APY
-                </div>
-            </td>
         </tr>
         <tr>
-            <td>
-                <a className={showpage === 3 ? "nav-link active" : "nav-link"} onClick={() => handlepage(3)}><img src="logo1.jpeg" /></a>
-            </td>
             <td><a href="https://app.aave.com/reserve-overview/?underlyingAsset=0xae78736cd615f374d3085123a210448e74fc6393&marketName=proto_mainnet_v3">AAVE</a></td>
             <td>
                 47.68K
