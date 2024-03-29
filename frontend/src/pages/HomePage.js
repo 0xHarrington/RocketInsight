d49839a4d1cc45b0ed306c24ad35a04e98c98ff0
0xHarrington <harringtonswim@gmail.com>
2024-03-29 16:22:15 -0400

    Adding content to the homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
-import LineGraph from '../components/Graph/LineGraph';
+import LineGraph from '../Components/LineGraph';
 import NavBar from '../components/Navigation/NavBar';
+import MarketTable from '../Components/MarketTable';
 import './style.css'; // Assuming style.css is moved to assets/styles
 
 const HomePage = () => {
@@ -14,7 +15,13 @@ const HomePage = () => {
             <NavBar showpage={showpage} handlepage={handlepage} />
             <Container>
                 <div className="page">
-                    {/* Page content goes here, using showpage to conditionally render components */}
+                    <h1>Welcome to RocketInsight</h1>
+                    <div className="subheading">
+                        <h3 align="center">Statistics, analytics and more, to help you make informed decisions with your rEth.</h3>
+                    </div>
+                    <LineGraph market="AAVE" timeframe={365} />
+                    <h2 align="center">Protocols with rEth Assets</h2>
+                    <MarketTable showpage={showpage} handlepage={handlepage} />
                 </div>
             </Container>
         </div>
