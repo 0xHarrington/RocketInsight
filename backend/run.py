27e30879f63853501783d82c6e9a84a8136dc522
Matt Harrington <harringtonswim@gmail.com>
2024-04-01 20:07:44 -0400

    string together new model separation

--- backend/run.py
+++ backend/run.py
@@ -1,12 +1,32 @@
-from flask import Flask
-from app.api import api
+from app.database import create_app, create_tables
+from app.seed import (
+    add_dataframe_to_db,
+    pd,
+)  # Assuming add_dataframe_to_db is correctly adjusted for this context
+from app.data_utils import scrape_historic_all  # Import the function that fetches data
 
-# Create the Flask application instance
-app = Flask(__name__)
+app = create_app()
 
-# Register the Blueprint with the Flask application
-app.register_blueprint(api, url_prefix='/api')
 
-# Optionally, you can configure the host and port in the run configuration
+def initialize_app():
+    """
+    Initialize the application, create tables, and seed the database.
+    """
+    # Create database tables
+    with app.app_context():
+        create_tables(app)
+        print("Database tables created.")
+
+        # Optionally, seed the database with initial data
+        # Note: Ensure that the scrape_historic_all function returns a DataFrame
+        # and add_dataframe_to_db is adapted to work with the app context correctly.
+        historical_data = scrape_historic_all(timeframe=365)
+        if add_dataframe_to_db(historical_data, HistoricalData):
+            print("Database seeded with historical data.")
+        else:
+            print("Error seeding database.")
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    initialize_app()
+    app.run(debug=True, host="0.0.0.0", port=5000)
