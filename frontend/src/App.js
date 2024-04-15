3a38497e7a52b5f503787528dea9aac3175a7c50
Deepak Nanjundan <Deepaknanjundan@gmail.com>
2024-04-15 16:20:58 -0400

    bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Pages from './Components/Pages';
+import React from 'react';
 
 function App() {
   return (
--- frontend/src/Components/graph.js
+++ frontend/src/Components/graph.js
@@ -1,68 +1,60 @@
-import React, { useState, useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import * as d3 from "d3";
+import { useEffect, useRef } from "react";
 
-const LineGraph = () => {
-  const [data, setData] = useState([]);
-  const svgRef = useRef();
+const Barchart = () => {
+  const ref = useRef();
 
   useEffect(() => {
-    fetchData();
+    // set the dimensions and margins of the graph
+    const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+      width = 460 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    const svg = d3
+      .select(ref.current)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Parse the Data
+    d3.csv(
+      "https://raw.githubusercontent.com/0xHarrington/RocketInsight/main/frontend/h_supply_borrow5.csv"
+    ).then(function (data) {
+      // X axis
+      const x = d3
+      .scaleBand()
+      .range([0, width])
+      .domain(data.map((d) => d.Date))
+      .padding(0.2);
+      svg
+        .append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+      // Add Y axis
+      const y = d3.scaleLinear().domain([0, 70000]).range([height, 0]);
+      svg.append("g").call(d3.axisLeft(y));
+
+      // Bars
+      svg
+        .selectAll("mybar")
+        .data(data)
+        .join("rect")
+        .attr("x", (d) => x(d.Date))
+        .attr("y", (d) => y(d.Total_Amount))
+        .attr("width", x.bandwidth())
+        .attr("height", (d) => height - y(d.Total_Amount))
+        .attr("fill", "#5f0f40");
+    });
   }, []);
 
-  useEffect(() => {
-    if (data.length === 0) return;
-
-    const svg = d3.select(svgRef.current);
-    const width = +svg.attr('width');
-    const height = +svg.attr('height');
-
-    // Define x and y scales
-    const xScale = d3.scaleLinear()
-      .domain(d3.extent(data, d => d.x))
-      .range([0, width]);
-
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.y)])
-      .range([height, 0]);
-
-    // Define line generator
-    const line = d3.line()
-      .x(d => xScale(d.x))
-      .y(d => yScale(d.y));
-
-    // Draw line
-    svg.append('path')
-      .datum(data)
-      .attr('class', 'line')
-      .attr('d', line)
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-width', 2);
-
-    // Draw x axis
-    svg.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(xScale));
-
-    // Draw y axis
-    svg.append('g')
-      .call(d3.axisLeft(yScale));
-  }, [data]);
-
-  const fetchData = () => {
-    d3.json('data.json').then(data => {
-      setData(data);
-    }).catch(error => {
-      console.error('Error fetching data:', error);
-    });
-  };
-
-  return (
-    <div>
-      <h2>Line Graph</h2>
-      <svg ref={svgRef} width={600} height={400}></svg>
-    </div>
-  );
+  return <svg width={1000} height={600} id="barchart" ref={ref} />;
 };
 
-export default LineGraph;
+export default Barchart;
