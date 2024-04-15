a53d0b40ef022350930763cf6cf77b02055c15ac
Matt Harrington <harringtonswim@gmail.com>
2024-04-15 16:36:00 -0400

    ahh, you have to register the API

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.models import db
+from app.api import api
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
     db.init_app(app)
+    app.register_blueprint(api, url_prefix="/api")
     return app
 
 
