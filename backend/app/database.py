181092b59d6f6fb40bcf1b38cc750112bf616e5f
Matt Harrington <harringtonswim@gmail.com>
2024-04-15 16:14:30 -0400

    just supply the database table file

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.models import db
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/data.db'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
     db.init_app(app)
     return app
+
 
 def create_tables(app):
     with app.app_context():
