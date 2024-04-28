c1a32168070c381603e03ed73964198076dc790d
Matt Harrington <harringtonswim@gmail.com>
2024-04-28 16:31:06 -0400

    prepping for last mile frontend<>backend

--- backend/app/api.py
+++ backend/app/api.py
@@ -27,89 +27,6 @@ def all_markets():
     return jsonify(markets_data)
 
 
-@api.route("/historical_leverage", methods=["GET"])
-def historical_leverage():
-    market = request.args.get("market", "mainnet aave v3")
-    timeframe = request.args.get("timeframe", "1 year")
-    # Here you would adjust your data fetching logic based on market and timeframe
-    leverage_data = {
-        "timestamp": "2023-01-01",
-        "leverage": 2.5,
-        "market": market,
-        "timeframe": timeframe,
-    }
-    return jsonify(leverage_data)
-
-
-@api.route("/supply_transactions", methods=["GET"])
-def supply_transactions():
-    market = request.args.get("market", "aave v3")
-    timeframe = request.args.get("timeframe", "1 year")
-
-    # Query supply transactions based on market and timeframe
-    supply_transactions = Transaction.query.filter_by(reserve=market).all()
-
-    # Initialize an empty list to store supply transaction data
-    transactions = []
-
-    # Process fetched supply transactions and append them to the transactions list
-    for transaction in supply_transactions:
-        transaction_data = {
-            "id": transaction.id,
-            "reserve": transaction.reserve,
-            "user": transaction.user,
-            "amount": transaction.amount,
-            "timestamp": transaction.timestamp,
-            "log_index": transaction.log_index,
-            "transaction_index": transaction.transaction_index,
-            "transaction_hash": transaction.transaction_hash,
-            "block_hash": transaction.block_hash,
-            "block_number": transaction.block_number,
-        }
-        transactions.append(transaction_data)
-
-    return jsonify(transactions)
-
-
-@api.route("/borrow_transactions", methods=["GET"])
-def borrow_transactions():
-    market = request.args.get("market", "aave v3")
-    timeframe = request.args.get("timeframe", "1 year")
-
-    # Query borrow transactions based on market and timeframe
-    borrow_transactions = Transaction.query.filter_by(reserve=market).all()
-
-    # Initialize an empty list to store borrow transaction data
-    transactions = []
-
-    # Process fetched borrow transactions and append them to the transactions list
-    for transaction in borrow_transactions:
-        transaction_data = {
-            "id": transaction.id,
-            "reserve": transaction.reserve,
-            "user": transaction.user,
-            "amount": transaction.amount,
-            "timestamp": transaction.timestamp,
-            "log_index": transaction.log_index,
-            "transaction_index": transaction.transaction_index,
-            "transaction_hash": transaction.transaction_hash,
-            "block_hash": transaction.block_hash,
-            "block_number": transaction.block_number,
-        }
-        transactions.append(transaction_data)
-
-    return jsonify(transactions)
-
-
-@api.route("/leveraged_users", methods=["GET"])
-def leveraged_users():
-    aum_threshold = request.args.get("AUM_threshold", 25)
-    market = request.args.get("market", "mainnet aave v3")
-    # Implement logic to fetch leveraged users based on AUM threshold and market
-    users = [{"user_id": "user1", "leverage_amount": 25}]
-    return jsonify(users)
-
-
 @api.route("/user_history", methods=["GET"])
 def user_history():
     user_addresses = request.args.getlist("user_address")
@@ -154,3 +71,87 @@ def user_history():
                 user_history_data.append(history_entry)
 
     return jsonify(user_history_data)
+
+
+## NOTE: Deprecated endpoints :(
+# @api.route("/historical_leverage", methods=["GET"])
+# def historical_leverage():
+#     market = request.args.get("market", "mainnet aave v3")
+#     timeframe = request.args.get("timeframe", "1 year")
+#     # Here you would adjust your data fetching logic based on market and timeframe
+#     leverage_data = {
+#         "timestamp": "2023-01-01",
+#         "leverage": 2.5,
+#         "market": market,
+#         "timeframe": timeframe,
+#     }
+#     return jsonify(leverage_data)
+
+
+# @api.route("/supply_transactions", methods=["GET"])
+# def supply_transactions():
+#     market = request.args.get("market", "aave v3")
+#     timeframe = request.args.get("timeframe", "1 year")
+
+#     # Query supply transactions based on market and timeframe
+#     supply_transactions = Transaction.query.filter_by(reserve=market).all()
+
+#     # Initialize an empty list to store supply transaction data
+#     transactions = []
+
+#     # Process fetched supply transactions and append them to the transactions list
+#     for transaction in supply_transactions:
+#         transaction_data = {
+#             "id": transaction.id,
+#             "reserve": transaction.reserve,
+#             "user": transaction.user,
+#             "amount": transaction.amount,
+#             "timestamp": transaction.timestamp,
+#             "log_index": transaction.log_index,
+#             "transaction_index": transaction.transaction_index,
+#             "transaction_hash": transaction.transaction_hash,
+#             "block_hash": transaction.block_hash,
+#             "block_number": transaction.block_number,
+#         }
+#         transactions.append(transaction_data)
+
+#     return jsonify(transactions)
+
+
+# @api.route("/borrow_transactions", methods=["GET"])
+# def borrow_transactions():
+#     market = request.args.get("market", "aave v3")
+#     timeframe = request.args.get("timeframe", "1 year")
+
+#     # Query borrow transactions based on market and timeframe
+#     borrow_transactions = Transaction.query.filter_by(reserve=market).all()
+
+#     # Initialize an empty list to store borrow transaction data
+#     transactions = []
+
+#     # Process fetched borrow transactions and append them to the transactions list
+#     for transaction in borrow_transactions:
+#         transaction_data = {
+#             "id": transaction.id,
+#             "reserve": transaction.reserve,
+#             "user": transaction.user,
+#             "amount": transaction.amount,
+#             "timestamp": transaction.timestamp,
+#             "log_index": transaction.log_index,
+#             "transaction_index": transaction.transaction_index,
+#             "transaction_hash": transaction.transaction_hash,
+#             "block_hash": transaction.block_hash,
+#             "block_number": transaction.block_number,
+#         }
+#         transactions.append(transaction_data)
+
+#     return jsonify(transactions)
+
+
+# @api.route("/leveraged_users", methods=["GET"])
+# def leveraged_users():
+#     aum_threshold = request.args.get("AUM_threshold", 25)
+#     market = request.args.get("market", "mainnet aave v3")
+#     # Implement logic to fetch leveraged users based on AUM threshold and market
+#     users = [{"user_id": "user1", "leverage_amount": 25}]
+#     return jsonify(users)
